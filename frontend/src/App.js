07fb92c1c30b13e1f3d484d0ccd70b6d7b23e9a6
Taras Protsyshyn <protsyshyn.t@gmail.com>
2025-08-25 06:27:41 +0000

    Add product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { HomeScreen } from "./screens/HomeScreen";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main className="py-3">
         <Container>
           <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,33 @@
+import { Row, Col } from "react-bootstrap";
+import { Product } from "../components/Product";
+
+const products = [
+  {
+    _id: "1",
+    name: "Airpods Wireless Bluetooth Headphones",
+    image: "/images/airpods.jpg",
+    description:
+      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
+    brand: "Apple",
+    category: "Electronics",
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+];
+
+export const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
