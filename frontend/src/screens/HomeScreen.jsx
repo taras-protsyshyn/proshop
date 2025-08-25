d3089f6027f1efe60beff6f8fdf050fcd5ae62ce
Taras Protsyshyn <protsyshyn.t@gmail.com>
2025-08-25 07:35:18 +0000

    add product screen

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,21 +1,7 @@
 import { Row, Col } from "react-bootstrap";
 import { Product } from "../components/Product";
 
-const products = [
-  {
-    _id: "1",
-    name: "Airpods Wireless Bluetooth Headphones",
-    image: "/images/airpods.jpg",
-    description:
-      "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
-    brand: "Apple",
-    category: "Electronics",
-    price: 89.99,
-    countInStock: 10,
-    rating: 4.5,
-    numReviews: 12,
-  },
-];
+import { products } from "../assets/products";
 
 export const HomeScreen = () => {
   return (
