a882faa32877da62e29539915b2a2627c9badf3e
Taras Protsyshyn <protsyshyn.t@gmail.com>
2025-08-25 06:43:39 +0000

    add routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
+import { Outlet } from "react-router-dom";
+
 import { Container } from "react-bootstrap";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import { HomeScreen } from "./screens/HomeScreen";
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <main className="py-3">
         <Container>
           <h1>Welcome to ProShop</h1>
-          <HomeScreen />
+          <Outlet />
         </Container>
       </main>
       <Footer />
