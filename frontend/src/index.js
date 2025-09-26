2858712b547e439c8607a2edc071873c2742ead0
Taras Protsyshyn <protsyshyn.t@gmail.com>
2025-09-26 10:35:33 +0000

    Add cart screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -14,12 +14,14 @@ import "./assets/styles/index.css";
 import App from "./App";
 import { HomeScreen } from "./screens/HomeScreen";
 import { ProductScreen } from "./screens/ProductScreen";
+import { CartScreens } from "./screens/CartScreens";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} path="/" element={<HomeScreen />}></Route>
       <Route path="product/:id" element={<ProductScreen />}></Route>
+      <Route path="cart" element={<CartScreens />}></Route>
     </Route>
   )
 );
